8202531d42416f84c4eae27504b1400e9ae35b88
Anna Lezhikova <anna.v.l@gmail.com>
2018-02-05 10:33:13 +1300

    Add FilterHeader container, add filteres to init state, make setter getter for filters

--- src/state/reducer/index.js
+++ src/state/reducer/index.js
@@ -1,25 +1,57 @@
-import { EXTERNAL_DATA_LOAD_FAILED, EXTERNAL_DATA_LOADED } from '../constants'
+import { keys, fromPairs, pipe, tail, map, concat, assoc } from "ramda";
+import {
+  EXTERNAL_DATA_LOAD_FAILED,
+  EXTERNAL_DATA_LOADED,
+  FILTERS_UPDATED
+} from "../constants";
 
-const initialState = {}
+const initialState = {};
 
-const reducer = function (state = initialState, action) {
+const reducer = function(state = initialState, action) {
   switch (action.type) {
     case EXTERNAL_DATA_LOAD_FAILED: {
       return {
         ...state,
         externalDataLoadError: action.payload.externalDataLoadError
-      }
+      };
     }
     case EXTERNAL_DATA_LOADED: {
       return {
         ...state,
-        externalData: action.payload.externalData
-      }
+        externalData: action.payload.externalData,
+        filters: setupFilters(action.payload.externalData)
+      };
+    }
+    case FILTERS_UPDATED: {
+      return {
+        ...state,
+        filters: updateFilters(state.filters, action.payload.activeFilter)
+      };
     }
     default: {
-      return state
+      return state;
     }
   }
-}
+};
 
-export default reducer
+//This function can be moved into a separate file when the app is bigger
+const setupFilters = data => {
+  if (data) {
+    const fields = keys(data[0]); // here should be a data schema used
+    const filters = pipe(
+      map(field => [field, { isActive: false, direction: "down" }]),
+      concat([[fields[0], { isActive: true, direction: "down" }]]),
+      fromPairs
+    )(tail(fields));
+    return filters;
+  }
+  return [];
+};
+
+const updateFilters = (filters, activeFilterName) => {
+  const activeFilter = pick([activeFilterName], filters);
+  const updatedFilter = assoc("isActive", true, activeFilter);
+  return assoc(activeFilterName, updatedFilter, filters);
+};
+
+export default reducer;
