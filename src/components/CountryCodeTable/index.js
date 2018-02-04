ba6e9adfff89bc9af4c6389cbf024b94823e002d
Anna Lezhikova <anna.v.l@gmail.com>
2018-02-05 09:33:01 +1300

    Makes table to be undependent from data: columns are whatever data is

--- src/components/CountryCodeTable/index.js
+++ src/components/CountryCodeTable/index.js
@@ -1,58 +1,59 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { map } from 'ramda'
-import { Row, Table } from 'react-bootstrap'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { map, keys } from "ramda";
+import { Row, Table } from "react-bootstrap";
 
 class CountryCodeTable extends Component {
-  componentDidMount () {
-    this.props.initialiseApp()
+  componentDidMount() {
+    this.props.initialiseApp();
   }
 
-  getRows (data) {
+  getRows({ data, fields }) {
     return map(
-      ({ name, code, dialCode }) => (
-        <tr key={code}>
-          <td>{name}</td>
-          <td>{code}</td>
-          <td>{dialCode}</td>
-        </tr>
+      // row key has to be unique, but using Code field doesn't guarantee it.
+      // data index, which uniqueness guaranted by the DB is a good candidate for this
+      // "code" should be "id", to use it data structures needs a change
+      row => (
+        <tr key={row.code}>{map(field => <td>{row[field]}</td>)(fields)}</tr>
       ),
       data
-    )
+    );
   }
 
-  getTable (data) {
-    return data ? (
+  getTable({ data, fields }) {
+    return data ? ( // use data that was sent to the component
       <Table striped responsive>
         <thead>
-          <tr>
-            <th>Name</th>
-            <th>Code</th>
-            <th>Dial Code</th>
-          </tr>
+          <tr>{map(field => <th>{field}</th>)(fields)}</tr>
         </thead>
-        <tbody>{this.getRows(this.props.externalData)}</tbody>
+        <tbody>{this.getRows({ data, fields })}</tbody>
       </Table>
     ) : (
       <p>Loading data</p>
-    )
+    );
   }
 
-  render () {
+  render() {
+    const data = this.props.externalData;
+    const fields = data ? keys(data[0]) : []; //make tbale independable from the data
+    // now the columns are whatever fields in the data exist
+    // component doesn't have to now the names of the fields
+    // but the data has to be well structured (and has some schema)
+    // better data structure -> better code
     return (
       <div>
-        <Row key='header-row'>
+        <Row key="header-row">
           <h1>Country Calling Codes</h1>
         </Row>
-        <Row key='body-row'>{this.getTable(this.props.externalData)}</Row>,
+        <Row key="body-row">{this.getTable({ data, fields })}</Row>,
       </div>
-    )
+    );
   }
 }
 
 CountryCodeTable.propTypes = {
   externalData: PropTypes.array,
   initialiseApp: PropTypes.func.isRequired
-}
+};
 
-export default CountryCodeTable
+export default CountryCodeTable;
